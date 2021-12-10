19bec7d1ca0d6d2c16b8429373dbc2712a485fb4
alexis israel quezada palma <ipalm001@hotmail.com>
2021-12-10 16:08:15 -0300

    Ejercicios java

--- src/cl/desafiolatam/f20211210/ParametrosMain.java
+++ src/cl/desafiolatam/f20211210/ParametrosMain.java
@@ -13,7 +13,7 @@ public class ParametrosMain {
 	public static void sumar(String[] args) {
 		int suma = 0;
 		for (int i = 0; i < args.length; i++) {
-			System.out.println("parametro i=" + i + ", " + args[i]);
+			System.out.println("parametro i = " + i + ", " + args[i]);
 			suma = suma + Integer.parseInt(args[i]);
 		}
 		
