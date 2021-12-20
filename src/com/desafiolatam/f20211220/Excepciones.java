2fbc188907bd736d2afb54aa35a964921ec46aaf
alexis israel quezada palma <ipalm001@hotmail.com>
2021-12-20 14:39:42 -0300

    20211220 POO II excepciones 2

--- src/com/desafiolatam/f20211220/Excepciones.java
+++ src/com/desafiolatam/f20211220/Excepciones.java
@@ -1,38 +1,51 @@
 package com.desafiolatam.f20211220;
 
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Excepciones {
 
 	public static void main(String[] args) {
-	
+
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Ingrese un numero");
+
 		double division = 0;
-		
+
 		try {
-			 Integer numero = sc.nextInt();
-			 if(numero.equals(0)) {
-				 numero = null;
-			 }
-			 			 
-			division = 3/numero;
-			
+			System.out.println("Ingrese primer numero");
+			String numero = sc.nextLine();
+
+			System.out.println("Ingrese segundo numero");
+			String numero2 = sc.nextLine();
+			/*
+			 * if(numero.equals(0)) { numero = null; }
+			 */
+			division = Integer.parseInt(numero) / Integer.parseInt(numero2);
+
 		} catch (InputMismatchException e) {
 			System.out.println("Error de ingreso: " + e.getMessage());
+			throw new InputMismatchException(e.getMessage());
+			
 		} catch (ArithmeticException e) {
 			System.out.println("Error matematico: " + e.getMessage());
 			division = 0;
-		} catch (Exception e) {
-			System.out.println("Otro error: "+ e);
-		}
-		
-		System.out.println("El resultado de la division es: "+ division);
+			throw new ArithmeticException(e.getMessage());
+			
+		} catch (NumberFormatException nfe) {
+			System.out.println(" Number exception: " + nfe.getMessage());
 
-		sc.close();
+		} catch (NullPointerException e) {
+			System.out.println("Null pointer: " + e.getMessage());
+			throw new NullPointerException(e.getMessage());
+			
+		} catch (Exception e) {
+			System.out.println("Otro error: " + e);
+			
+		} finally {
+			System.out.println("En finally ...");
+			sc.close();
+		}
+		System.out.println("El resultado de la division es: " + division);
 	}
 
 }
