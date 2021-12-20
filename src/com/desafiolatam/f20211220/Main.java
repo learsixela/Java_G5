6606ec6ed330c5a3041ba2c7ba3d8fdfabb41da3
alexis israel quezada palma <ipalm001@hotmail.com>
2021-12-20 11:09:31 -0300

    20211220 POO II ejemplo aceleracion

--- src/com/desafiolatam/f20211220/Main.java
+++ src/com/desafiolatam/f20211220/Main.java
@@ -7,11 +7,12 @@ public class Main {
 	public static void main(String[] args) {
 		//instancia de Auto
 		Auto auto = new Auto();
+		System.out.println(auto.toString());
 		//aumenta automaticamente en 10
 		auto.aumentarVelocidad();
 		System.out.println(auto.toString());
 		//aceleramos con un valor especifico
-		auto.aumentarVelocidad(5);
+		auto.aumentarVelocidad(-14);
 		System.out.println(auto.toString());
 		//frenamos
 		auto.aumentarVelocidad(false, true);
--- src/com/desafiolatam/f20211220/models/Auto.java
+++ src/com/desafiolatam/f20211220/models/Auto.java
@@ -1,73 +1,89 @@
 package com.desafiolatam.f20211220.models;
 
 public class Auto {
-	//atributos
-	private String marca; 
+	// atributos
+	private String marca;
 	private Float motor;
 	private String color;
 	private float velocidad;
 
-	//constructores
-	public Auto(String marca, Float motor, String color, float velocidad ) {
+	// constructores
+	public Auto(String marca, Float motor, String color, float velocidad) {
 		super();
 		this.marca = marca;
 		this.motor = motor;
 		this.color = color;
-		this.velocidad= velocidad;
+		this.velocidad = velocidad;
 	}
+
 	public Auto() {
 		super();
 	}
-	
-	//getters&Setters
+
+	// getters&Setters
 	public float getVelocidad() {
 		return velocidad;
 	}
+
 	public void setVelocidad(float velocidad) {
 		this.velocidad = velocidad;
 	}
+
 	public String getMarca() {
 		return marca;
 	}
+
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
+
 	public Float getMotor() {
 		return motor;
 	}
+
 	public void setMotor(Float motor) {
 		this.motor = motor;
 	}
+
 	public String getColor() {
 		return color;
 	}
+
 	public void setColor(String color) {
 		this.color = color;
 	}
-	//metodos
+
+	// metodos
 	@Override
 	public String toString() {
-		return "Auto [marca=" + marca + ", motor=" + motor + ", color=" + color + "]";
+		return "Auto [marca=" + marca + ", motor=" + motor + ", color=" + color + ", velocidad=" + velocidad + "]";
 	}
-	
+
 	public void aumentarVelocidad() {
 		velocidad = velocidad + 10;
-		System.out.println("La velocidad es: "+velocidad);
+		System.out.println("La velocidad es: " + velocidad);
 	}
-	
+
 	public void aumentarVelocidad(float velocidad) {
-		this.velocidad = this.velocidad + velocidad;
-		System.out.println("La velocidad es: "+this.velocidad);
-	}
-	
-	public void aumentarVelocidad(boolean acelerar, boolean frenar) {
-		if(acelerar) {
-			velocidad = velocidad + 15;
-		}else if(frenar) {
-			velocidad = velocidad - 15;
+		if(velocidad > 0) {
+			this.velocidad = this.velocidad + velocidad;
 		}
 		
-		System.out.println("La velocidad es: "+velocidad);
+		System.out.println("La velocidad es: " + this.velocidad);
 	}
-	
+
+	public void aumentarVelocidad(boolean acelerar, boolean frenar) {
+		if (acelerar) {
+			velocidad = velocidad + 15;
+		} else if (frenar) {
+			if (velocidad < 15) {
+				velocidad = 0;
+			} else {
+				velocidad = velocidad - 15;
+			}
+		}
+
+		System.out.println("La velocidad es: " + velocidad);
+	}
+
 }
