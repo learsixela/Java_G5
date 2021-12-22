f785951393d0b4e54974568805d19d132d0ea0d5
alexis israel quezada palma <ipalm001@hotmail.com>
2021-12-22 11:56:35 -0300

    20211222 POO Polimorfismo I, lista

--- src/com/desafiolatam/f20211222/Main.java
+++ src/com/desafiolatam/f20211222/Main.java
@@ -1,5 +1,7 @@
 package com.desafiolatam.f20211222;
 
+import java.util.ArrayList;
+
 import com.desafiolatam.f20211222.models.Cardiologo;
 import com.desafiolatam.f20211222.models.Medico;
 import com.desafiolatam.f20211222.models.Neurologo;
@@ -10,21 +12,33 @@ public class Main {
 	public static void main(String[] args) {
 		//instancia de medico
 		Medico medico = new Medico("House","1-9",55);
-		medico.consulta();
+		//medico.consulta();
 		
 		/**
 		 * Polimorfismo
 		 * */
 		Medico neurologo = new Neurologo("Tom","74574",40,"Neurologia");
-		neurologo.consulta();
+		//neurologo.consulta();
 		
 		Medico cardiologo = new Cardiologo("Clara","46455", 37, "Cardiologia");
-		cardiologo.consulta();
+		//cardiologo.consulta();
 		
 		Medico oftalmologo = new Oftalmologo("Dina","74582", 42, "Oftalmologia");
-		oftalmologo.consulta();
+		//oftalmologo.consulta();
 		
+		Oftalmologo of = new Oftalmologo();
 		
+		//Arreglo de Medico
+		ArrayList<Medico> listaMedicos = new ArrayList<Medico>();
+		listaMedicos.add(medico);
+		listaMedicos.add(neurologo);
+		listaMedicos.add(cardiologo);
+		listaMedicos.add(oftalmologo);
+		listaMedicos.add(of);
+		
+		for (Medico medico2 : listaMedicos) {
+			medico2.consulta();
+		}		
 	}
 
 }
