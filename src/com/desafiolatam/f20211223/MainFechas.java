4980026ae793064eb7077f0215f3ce81a6cf1ad9
alexis israel quezada palma <ipalm001@hotmail.com>
2021-12-23 11:38:27 -0300

    2021123 Repaso fechas

--- src/com/desafiolatam/f20211223/MainFechas.java
+++ src/com/desafiolatam/f20211223/MainFechas.java
@@ -14,6 +14,40 @@ public class MainFechas {
 		 * Utilizar substring de la clase String.*/
 		
 		//					30/12/2021 
-		System.out.println(fecha1.substring(6));//(0,2); (3,5); (6)
+		//System.out.println(fecha1.substring(6));//(0,2); (3,5); (6)
+		int dia1 = Integer.parseInt(fecha1.substring(0,2));//30
+		int mes1 = Integer.parseInt(fecha1.substring(3,5));//12
+		int anio1 = Integer.parseInt(fecha1.substring(6));//2021
+		
+		int dia2 = Integer.parseInt(fecha2.substring(0,2));
+		int mes2 = Integer.parseInt(fecha2.substring(3,5));
+		int anio2 = Integer.parseInt(fecha2.substring(6));
+		
+		 // si el año es menor, la fecha es mayor 
+		
+		if(anio1 > anio2) {//2000  > 1980
+			System.out.println("Fecha 2 es mas antigua ");
+		}else if(anio1 < anio2) {//1980  < 2000
+			System.out.println("Fecha 1 es mas antigua ");
+		}else {//años iguales
+			if(mes1 > mes2) {
+				System.out.println("fecha 2 es mayor");
+			}else if(mes1 < mes2) {
+				System.out.println("fecha 1 es mayor");
+			}else {//meses iguales
+				if(dia1 > dia2) {
+					System.out.println("fecha 2 es mayor");
+				}else if(dia1 < dia2) {// 2 < 25
+					System.out.println("fecha 1 es mayor");
+				}else {//dias iguales
+					System.out.println("Es la misma fecha");
+				}
+			}
+		}
+		//20/12/2021 fecha 1
+		//20/12/2021 fecha 2 
+
+		//2000/12/12
+		
 	}
 }
