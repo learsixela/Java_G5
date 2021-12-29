92b6becea0162d02f0c7f634f6ecf96ba479e337
alexis israel quezada palma <ipalm001@hotmail.com>
2021-12-29 14:39:30 -0300

    20211229 castings calculo area

--- src/com/desafiolatam/f20211229/CalculoArea.java
+++ src/com/desafiolatam/f20211229/CalculoArea.java
@@ -1,10 +1,19 @@
 package com.desafiolatam.f20211229;
 
+import java.util.Scanner;
+
 public class CalculoArea {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ingrese el alto del rectangulo");
+		String alto= sc.nextLine();
+		System.out.println("Ingrese el largo del rectangulo");
+		String largo= sc.nextLine();
+		//area=a*l;
+		int area = Integer.parseInt(alto) * Integer.parseInt(largo);
 
+		System.out.println("El area del rectangulo es: "+ area);
 	}
 
 }
